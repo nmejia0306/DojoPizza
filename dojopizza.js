f853098903191fd0e952bd1845e46bc8310f1ecf
nmejia0306 <76713448+nmejia0306@users.noreply.github.com>
2021-05-24 13:53:44 -0500

    JavaScript obejcts

---
+++ dojopizza.js
@@ -0,0 +1,57 @@
+var pizza = {
+    crustType: 'deep dish',
+    sauseType: 'traditional',
+    chesses: ['mozzarella'],
+    toppings: ['pepperoni, sausage'],
+    pizzaOven: function(){
+    console.log(`Crust: ${pizza.crustType}`)
+    console.log(`Sause: ${pizza.sauseType}`)
+    console.log(`Chesses: ${pizza.chesses}`)
+    console.log(`Toppings ${pizza.toppings}`)
+    }
+}
+pizza.pizzaOven()
+
+var pizza2 = {
+    crustType: 'hand tossed',
+    sauseType: 'marinara',
+    chesses: ['mozzarella , feta'],
+    toppings: ['mushrooms, olives , and onions'],
+    pizzaOven: function(){
+    console.log(`Crust: ${pizza2.crustType}`)
+    console.log(`Sause: ${pizza2.sauseType}`)
+    console.log(`Chesses: ${pizza2.chesses}`)
+    console.log(`Toppings ${pizza2.toppings}`)
+    }
+}
+pizza2.pizzaOven()
+
+var pizza3 = {
+    crustType: 'thin crust',
+    sauseType: 'marinara',
+    chesses: ['mozzarella '],
+    toppings: ['pepperoni, bacon, and extra chesse'],
+    pizzaOven: function(){
+    console.log(`Crust: ${pizza3.crustType}`)
+    console.log(`Sause: ${pizza3.sauseType}`)
+    console.log(`Chesses: ${pizza3.chesses}`)
+    console.log(`Toppings ${pizza3.toppings}`)
+    }
+}
+pizza3.pizzaOven()
+
+var pizza4 = {
+    crustType: 'thin crust ',
+    sauseType: 'mayo',
+    chesses: ['mozzarella'],
+    toppings: ['lettuce, tomatos, and bacon'],
+    pizzaOven: function(){
+    console.log(`Crust: ${pizza4.crustType}`)
+    console.log(`Sause: ${pizza4.sauseType}`)
+    console.log(`Chesses: ${pizza4.chesses}`)
+    console.log(`Toppings ${pizza4.toppings}`)
+    }
+}
+pizza4.pizzaOven()
+
+
